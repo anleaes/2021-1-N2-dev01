efd080337d2e25fed04d79fac6b5cc0dc97b4b9c
eduardalutz <eduarda.lutz.silva@gmail.com>
2021-06-26 16:24:11 -0300

    ajuste parte de login

--- SuV_App/apps/vaccines/models.py
+++ SuV_App/apps/vaccines/models.py
@@ -3,11 +3,15 @@ from categories.models import Category
 
 # Create your models here.
 class Vaccine(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     name = models.CharField('Nome', max_length=50)
     description = models.TextField('Descricao', max_length=100)
     date_fabrication = models.DateField('Data Fabricacao', auto_now=False, auto_now_add=False) 
     expiration_date = models.DateField('Data Vencimento', auto_now=False, auto_now_add=False) 
     is_active = models.BooleanField('Ativo', default=False)
+    photo = models.ImageField('Foto', upload_to='photos')
+    doc = models.FileField('Documentos', upload_to='docs')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     class Meta:
