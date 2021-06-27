7b64dab55c7722b0c251cf11978a2eef7d52aaae
adamcardoso <adamjuniorcardoso@gmail.com>
2021-06-27 16:00:29 -0300

    atualizando código e corrigindo bugs

--- SuV_App/SuV_App/urls.py
+++ SuV_App/SuV_App/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
@@ -28,5 +31,4 @@ urlpatterns = [
     path('vacinacao/', include('vaccinations.urls', namespace='vaccinations')),
 ]
 
-
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
